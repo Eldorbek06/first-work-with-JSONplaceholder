679bef738f94e273fdb74b36132c84b1f636e3b1
Eldor06 <eldorergashev20sm>
2023-04-01 18:04:22 +0500

    second commit

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-let data = []
+let base_url = "http://localhost:9120/users"
 let to_25 = document.querySelector('.users_lists .to_25 .list')
 let to_50 = document.querySelector('.users_lists .to_50 .list')
 let rest_users = document.querySelector('.users_lists .rest .list')
-fetch('https://dummyjson.com/users')
-    .then(res => res.json())
-    .then(res => reload(res.users, to_25, to_50, rest_users));
+let form = document.forms.main
+let inps = document.querySelectorAll('input')
+
+function reFetch() {
+    fetch(base_url)
+        .then(res => res.json())
+        .then(res => reload(res, to_25, to_50, rest_users));
+}
+reFetch()
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+    let user = { "image": "https://cordis.europa.eu/docs/news/images/2023-03/442991.jpg" }
+    let fm = new FormData(form)
+    fm.forEach((v, k) => user[k] = v)
+    fetch(base_url, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-type": "application/json" }
+    })
+        .then(res => res.json())
+        .then(res => reFetch())
+}
 
 function reload(arr, place1, place2, place3) {
     place1.innerHTML = ''
@@ -24,6 +44,7 @@ function reload(arr, place1, place2, place3) {
         user_bottom.classList.add('bottom', 'item')
         user_bottom_age.classList.add('age')
         user_bottom_age_num.classList.add('age_num')
+        user.style.background = colorRandomizer()
 
         user_top_img.src = item.image
         user_top_img.alt = 'image'
@@ -42,16 +63,27 @@ function reload(arr, place1, place2, place3) {
         } else {
             place3.append(user)
         }
+        user_top_img.onclick = () => {
+            fetch(base_url + '/' + item.id, {
+                method: "DELETE",
+                headers: { "Content-type": "application/json" }
+            }).then(reFetch())
+        }
     }
-    setTimeout(() => {
-        if(place1.lastElementChild){
-            place1.lastElementChild.classList.add('MB')
-        }
-        if(place2.lastElementChild){
-            place2.lastElementChild.classList.add('MB')
-        }
-        if(place3.lastElementChild){
-            place3.lastElementChild.classList.add('MB') 
-        }
-    });
+    if (place1.lastElementChild) {
+        place1.lastElementChild.classList.add('MB')
+    }
+    if (place2.lastElementChild) {
+        place2.lastElementChild.classList.add('MB')
+    }
+    if (place3.lastElementChild) {
+        place3.lastElementChild.classList.add('MB')
+    }
+}
+
+function colorRandomizer() {
+    let r = Math.round(Math.random() * 255)
+    let g = Math.round(Math.random() * 255)
+    let b = Math.round(Math.random() * 255)
+    return `rgb(${r}, ${g}, ${b})`
 }
